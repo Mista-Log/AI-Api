622aa1fc5b3b8c48e3cd7b572a484f24542f00e5
Mista Log <oloyedeoibrahimsmile@gmail.com>
2025-03-12 17:36:03 +0100

    new commit

--- SolanaAI/settings.py
+++ SolanaAI/settings.py
@@ -49,6 +49,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',  # Allow form-encoded data
+    ]
 }
 
 MIDDLEWARE = [
