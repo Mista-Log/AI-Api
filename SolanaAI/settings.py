f6912f6e7f2e90edbd9b0e714194621986593671
Mista Log <oloyedeoibrahimsmile@gmail.com>
2025-03-12 17:14:00 +0100

    new commit

--- SolanaAI/settings.py
+++ SolanaAI/settings.py
@@ -51,6 +51,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
     ]
 }
 
